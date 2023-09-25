3ccad5ad3cb42ad954c7f67ca86a730dfa2be6db
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-09-26 00:28:08 +0300

    Remove task comments

--- src/main/java/sample/DateSorter.java
+++ src/main/java/sample/DateSorter.java
@@ -8,13 +8,6 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-/**
- * Marking will be based upon producing a readable, well engineered solution rather than factors
- * such as speed of processing or other performance-based optimizations, which are less
- * important.
- *
- * Implement in single class. Don't chane constructor and signature method.
- */
 public class DateSorter {
     public static class SubsetSortRule {
         private final Predicate<LocalDate> subsetDetectionPredicate;
@@ -54,22 +47,6 @@ public class DateSorter {
         this.subsetSortRules = subsetSortRules;
     }
 
-    /**
-     * The implementation of this method should sort dates.
-     * The output should be in the following order:
-     * Dates with an 'r' in the month,
-     * sorted ascending (first to last),
-     * then dates without an 'r' in the month,
-     * sorted descending (last to first).
-     * For example, October dates would come before May dates,
-     * because October has an 'r' in it.
-     * thus: (2004-07-01, 2005-01-02, 2007-01-01, 2032-05-03)
-     * would sort to
-     * (2005-01-02, 2007-01-01, 2032-05-03, 2004-07-01)
-     *
-     * @param unsortedDates - an unsorted list of dates
-     * @return the collection of dates now sorted as per the spec
-     */
     public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
         Stream<Stream<LocalDate>> sortedSubsets = extractSortedSubsets(unsortedDates);
         return concatSubsets(sortedSubsets);
