55f59ca0ca0c1d5b892e2b7e2da153ceaca5ce1c
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-09-26 16:39:35 +0300

    Add more tests for specific cases

--- src/test/java/sample/DateSorterTest.java
+++ src/test/java/sample/DateSorterTest.java
@@ -1,21 +1,24 @@
 package sample;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class DateSorterTest {
-    private final DateSorter dateSorter = new DateSorter();
+    private DateSorter dateSorter;
 
-    /*
-     * (2004-07-01, 2005-01-02, 2007-01-01, 2032-05-03)
-     * would sort to
-     * (2005-01-02, 2007-01-01, 2032-05-03, 2004-07-01)
-     */
+    @BeforeEach
+    void setUp() {
+        dateSorter = new DateSorter();
+    }
+
     @Test
     void exampleDataTest() {
         List<LocalDate> unsortedDates = List.of(
@@ -34,5 +37,66 @@ class DateSorterTest {
                 LocalDate.of(2004, 7, 1)
         );
         assertThat(actualSortedDates).containsExactlyElementsOf(expectedSortedDates);
+    }
+
+    @Test
+    void datesNotBelongingToAnySubsetArePlacedInTheEnd() {
+        List<DateSorter.SubsetRule> subsetRules = List.of(
+                new DateSorter.SubsetRule(this::isBefore2007, Comparator.reverseOrder())
+        );
+        dateSorter.setSubsetSortRules(subsetRules);
+
+        List<LocalDate> unsortedDates = List.of(
+                LocalDate.of(2004, 7, 1),
+                LocalDate.of(2005, 1, 2),
+                LocalDate.of(2007, 1, 1),
+                LocalDate.of(2032, 5, 3)
+        );
+        Collection<LocalDate> actualSortedDates = dateSorter.sortDates(unsortedDates);
+
+        List<LocalDate> expectedSortedDates = List.of(
+                LocalDate.of(2005, 1, 2), // subset 1
+                LocalDate.of(2004, 7, 1), // subset 1
+                LocalDate.of(2007, 1, 1), // no subset
+                LocalDate.of(2032, 5, 3)  // no subset
+        );
+        assertThat(actualSortedDates).containsExactlyElementsOf(expectedSortedDates);
+    }
+
+    private boolean isBefore2007(LocalDate date) {
+        return date.isBefore(LocalDate.of(2007, 1, 1));
+    }
+
+    @Test
+    void dateBelongsOnlyToTheFirstSuitableSubset() {
+        List<DateSorter.SubsetRule> subsetRules = List.of(
+                new DateSorter.SubsetRule(this::is2005, Comparator.naturalOrder()),
+                new DateSorter.SubsetRule(this::isJanuary, Comparator.reverseOrder())
+        );
+        dateSorter.setSubsetSortRules(subsetRules);
+
+        List<LocalDate> unsortedDates = List.of(
+                LocalDate.of(2004, 1, 1),
+                LocalDate.of(2005, 1, 2),
+                LocalDate.of(2007, 1, 1),
+                LocalDate.of(2032, 1, 3)
+        );
+        Collection<LocalDate> actualSortedDates = dateSorter.sortDates(unsortedDates);
+
+        List<LocalDate> expectedSortedDates = List.of(
+                LocalDate.of(2005, 1, 2), // subset 1
+                LocalDate.of(2032, 1, 3), // subset 2
+                LocalDate.of(2007, 1, 1), // subset 2
+                LocalDate.of(2004, 1, 1)  // subset 2
+        );
+        assertThat(actualSortedDates).containsExactlyElementsOf(expectedSortedDates);
+    }
+
+    private boolean is2005(LocalDate date) {
+        return date.getYear() == 2005;
+    }
+
+    private boolean isJanuary(LocalDate date) {
+        return date.getMonth().equals(Month.JANUARY);
     }
 }
