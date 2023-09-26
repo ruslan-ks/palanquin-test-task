05234cc8039924c219a66a9acf513ce7ef9b7aee
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-09-26 16:40:04 +0300

    Fix DateSorter so it passes new tests; refactor

--- src/main/java/sample/DateSorter.java
+++ src/main/java/sample/DateSorter.java
@@ -1,38 +1,45 @@
 package sample;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
-import java.util.function.Function;
+import java.util.*;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class DateSorter {
-    public static class SubsetSortRule {
+    public static class SubsetRule {
         private final Predicate<LocalDate> belongsToSubset;
         private final Comparator<LocalDate> subsetSortComparator;
 
         /**
-         * @param belongsToSubset defines whether an item belongs to this subset
-         * @param subsetSortComparator Comparator used to sort an items inside the subset
+         * @param belongsToSubset      Defines whether an item belongs to this subset
+         * @param subsetSortComparator Comparator used to sort items inside the subset
          */
-        public SubsetSortRule(Predicate<LocalDate> belongsToSubset,
-                              Comparator<LocalDate> subsetSortComparator) {
+        public SubsetRule(Predicate<LocalDate> belongsToSubset, Comparator<LocalDate> subsetSortComparator) {
             this.belongsToSubset = belongsToSubset;
             this.subsetSortComparator = subsetSortComparator;
         }
     }
 
-    private static final Predicate<LocalDate> MONTH_NAME_CONTAINS_R = date -> monthNameContainsLetter(date, 'r');
+    // Defines how to sort dates that don't belong to any subset.
+    private static final SubsetRule FALLBACK_RULE = new SubsetRule(date -> true, Comparator.naturalOrder());
 
-    // 2 subsets by default (according to the task):
-    // First contains dates with month containing 'r' letter and is ascending
-    // Second contains all the other dates in descending order
-    private List<SubsetSortRule> subsetSortRules = List.of(
-            new SubsetSortRule(MONTH_NAME_CONTAINS_R, Comparator.naturalOrder()),
-            new SubsetSortRule(MONTH_NAME_CONTAINS_R.negate(), Comparator.reverseOrder())
-    );
+    private List<SubsetRule> subsetRules;
+
+    public DateSorter() {
+        setTaskSubsetRules();
+    }
+
+    private void setTaskSubsetRules() {
+        Predicate<LocalDate> monthNameContainsR = date -> monthNameContainsLetter(date, 'r');
+
+        // First dates with month containing 'r' letter in ascending order
+        // Then all the other dates in descending order
+        List<SubsetRule> taskRules = List.of(
+                new SubsetRule(monthNameContainsR, Comparator.naturalOrder()),
+                new SubsetRule(monthNameContainsR.negate(), Comparator.reverseOrder())
+        );
+        setSubsetSortRules(taskRules);
+    }
 
     private static boolean monthNameContainsLetter(LocalDate date, char letter) {
         String uppercaseLetter = String.valueOf(letter).toUpperCase();
@@ -40,37 +47,56 @@ public class DateSorter {
     }
 
     /**
-     * @param subsetSortRules the rules that define how to sort each subset;
-     *                        sorted subsets will be concatenated in order of rule occurrence
+     * @param subsetRules Rules that define how to sort each subset.
+     *                    Date object belongs to the first subset whose {@link SubsetRule#belongsToSubset}
+     *                    returns true.
+     *                    Sorted subsets will be concatenated in order of rule occurrence.
+     *                    Dates that don't belong to any subset will be placed in the end and sorted with
+     *                    {@link DateSorter#FALLBACK_RULE}
      */
-    public void setSubsetSortRules(List<SubsetSortRule> subsetSortRules) {
-        this.subsetSortRules = subsetSortRules;
+    public void setSubsetSortRules(List<SubsetRule> subsetRules) {
+        this.subsetRules = new ArrayList<>(subsetRules);
+        this.subsetRules.add(FALLBACK_RULE);
     }
 
     /**
-     * Sort dates according to the subsetSortRules
+     * Sort dates according to subsetRules
      * @param unsortedDates - an unsorted list of dates
      * @return the collection of dates now sorted as per the spec
      */
     public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
-        Stream<Stream<LocalDate>> sortedSubsets = extractSortedSubsets(unsortedDates);
+        List<LocalDate> datesCopy = new ArrayList<>(unsortedDates);
+
+        List<List<LocalDate>> sortedSubsets = extractSortedSubsets(datesCopy);
         return concatSubsets(sortedSubsets);
     }
 
-    private Stream<Stream<LocalDate>> extractSortedSubsets(List<LocalDate> dates) {
-        return subsetSortRules.stream()
-                .map(subsetSortRule -> extractAndSortSubset(dates, subsetSortRule));
+    private List<List<LocalDate>> extractSortedSubsets(List<LocalDate> dates) {
+        return subsetRules.stream()
+                .map(subsetRule -> extractAndSortSubset(dates, subsetRule))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    private Stream<LocalDate> extractAndSortSubset(List<LocalDate> dates, SubsetSortRule subsetSortRule) {
-        return dates.stream()
-                .filter(subsetSortRule.belongsToSubset)
-                .sorted(subsetSortRule.subsetSortComparator);
+    private List<LocalDate> extractAndSortSubset(List<LocalDate> dates, SubsetRule subsetRule) {
+        List<LocalDate> subset = extractSubset(dates, subsetRule);
+        subset.sort(subsetRule.subsetSortComparator);
+        return subset;
     }
 
-    private <T> List<T> concatSubsets(Stream<Stream<T>> subsets) {
-        return subsets
-                .flatMap(Function.identity())
+    /**
+     * Extract date objects from dates (updating list) into new subset according to subsetRule
+     */
+    private List<LocalDate> extractSubset(List<LocalDate> dates, SubsetRule subsetRule) {
+        List<LocalDate> subset = dates.stream()
+                .filter(subsetRule.belongsToSubset)
+                .collect(Collectors.toCollection(ArrayList::new));
+        dates.removeAll(subset);
+        return subset;
+    }
+
+    private List<LocalDate> concatSubsets(List<List<LocalDate>> subsets) {
+        return subsets.stream()
+                .flatMap(Collection::stream)
                 .toList();
     }
 }
